60137a09964e2b3bc171cf5534bab800082bd2f9
taisan <131413638+taisan11@users.noreply.github.com>
2024-02-19 18:18:55 +0900

    ka

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,9 +4,10 @@ import Counter from '../islands/counter'
 export default createRoute((c) => {
   return c.render(
     <div>
-      <h1>Hello, Hono!</h1>
-      <Counter />
+      <h1>Och</h1>
+      <p>0ではないOな掲示板を</p>
+      <a href="./top">入口</a>
     </div>,
-    { title: "Hono" }
+    { title: "Och" }
   )
 })
