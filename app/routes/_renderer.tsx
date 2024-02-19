7feeddf6bfd0fe38b928d8b306dc420be23a804d
taisan <131413638+taisan11@users.noreply.github.com>
2024-02-19 17:37:25 +0900

    a

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -4,13 +4,13 @@ import { Script } from 'honox/server'
 
 export default jsxRenderer(({ children, title }) => {
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
-        <Script src="/app/client.ts" />
-        <Style />
+        <Script src="/app/client.ts" async />
+        <link rel="stylesheet" href="/app/css/style.css" />
       </head>
       <body>{children}</body>
     </html>
