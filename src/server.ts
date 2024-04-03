3574ce4ec887b4e0ff778ed7da3aab8a864d8bda
taisan <131413638+taisan11@users.noreply.github.com>
2024-04-03 11:34:26 +0900

    ユーザー体験の向上

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/bun'
+import { trimTrailingSlash } from 'hono/trailing-slash'
 import BBS from './BBS'
 
 const app = new Hono()
 
+app.use(trimTrailingSlash())
 app.route("/", BBS);
 app.get('*', serveStatic({root: './html'}))
 export default app
