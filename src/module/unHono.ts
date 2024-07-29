b601129b07b7d557670071ab35c88a7b9f9e4e2f
taisan11 <131413638+taisan11@users.noreply.github.com>
2024-07-29 17:32:04 +0900

    add deno support

--- src/module/unHono.ts
+++ src/module/unHono.ts
@@ -1,16 +1,14 @@
 import { getConnInfo as bunconn } from 'hono/bun'
-import { getConnInfo as denoconn } from 'hono/deno'
 import { getConnInfo as cfconn } from 'hono/cloudflare-workers'
 import { runtime } from "std-env"
 import { Context } from 'hono'
 
 export function getConnInfo(c:Context) {
-    // if (runtime === 'deno') {
-    //     return denoconn(c)
-    // } else 
+    if (runtime === 'deno') {
+        return import('hono/deno').then((m) => m.getConnInfo(c))
+    } 
     if (runtime === 'workerd') {
         return cfconn(c)
-    } else {
-        return bunconn(c)
     }
+    return bunconn(c)
 }
