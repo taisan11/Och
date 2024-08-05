eb9fc598522adb46f82abb61fdf1cc48169127d3
taisan11 <131413638+taisan11@users.noreply.github.com>
2024-08-05 13:47:25 +0900

    last

--- src/server.deno.ts
+++ src/server.deno.ts
@@ -1,33 +1,4 @@
-import { Hono } from 'hono'
-import { trimTrailingSlash } from 'hono/trailing-slash'
-import { logger } from 'hono/logger'
-import { cors } from 'hono/cors'
-import { etag } from 'hono/etag'
-import { secureHeaders } from 'hono/secure-headers'
-import BBS from './UI'
-import API from './api'
-import OldUI from './oldui'
-import admin from './admin'
+import app from "./server"
 // import bbsmenuJson from './module/bbsmenu'
-
-const app = new Hono()
-
-app.use(trimTrailingSlash())
-const customLogger = (message: string, ...rest: string[]) => {
-    console.log(message, ...rest)
-}
-
-app.use(logger(customLogger))
-// app.use(compress())
-app.use(cors())
-// app.use(csrf())
-app.use(etag())
-app.use(secureHeaders())
-
-app.route("/admin", admin);
-app.route("/", OldUI);
-app.route("/api",API)
-app.route("/", BBS);
-
 
 Deno.serve(app.fetch)
